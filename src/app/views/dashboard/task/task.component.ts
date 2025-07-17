1e07045ab37907980f26a1d1c54bd9b9e6b20cb7
feumoeouvaneauxilia <feumoeouvane@gmail.com>
2025-07-17 17:10:29 +0100

    feat: implement user management in task component with user name resolution

--- src/app/views/dashboard/task/task.component.ts
+++ src/app/views/dashboard/task/task.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AlignDirective, BorderDirective, CardBodyComponent, CardComponent, CardHeaderComponent, ColComponent, RowComponent, TableActiveDirective, TableColorDirective, TableDirective } from '@coreui/angular';
 // import { DocsExampleComponent } from '@docs-components/public-api';
 import { TaskService } from '../../../services/task/task.service';
+import { UserService } from '../../../services/user/user.service';
 
 
 import { CommonModule } from '@angular/common';
@@ -27,12 +28,25 @@ import { CommonModule } from '@angular/common';
 })
 export class TaskComponent implements OnInit {
   tasks: any[] = [];
+  userNames: { [id: string]: string } = {};
 
-  constructor(private taskService: TaskService) {}
+  constructor(private taskService: TaskService, private userService: UserService) {}
 
   ngOnInit(): void {
     this.taskService.getTasks().subscribe((data: any) => {
       this.tasks = Array.isArray(data) ? data : (data?.tasks || []);
+      const userIds = new Set<string>();
+      this.tasks.forEach(task => {
+        if (task.assignedById) userIds.add(task.assignedById);
+        if (task.createdById) userIds.add(task.createdById);
+      });
+      userIds.forEach(id => {
+        if (id && !this.userNames[id]) {
+          this.userService.getUserById(id).subscribe(user => {
+            this.userNames[id] = user?.username || id;
+          });
+        }
+      });
     });
   }
 }
